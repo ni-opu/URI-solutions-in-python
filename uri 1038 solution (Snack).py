e51611b1599183509fef5b866bc5ae5b1904600e
ni-opu <67326422+ni-opu@users.noreply.github.com>
2021-07-19 21:42:44 +0600

    Update uri 1038 solution (Snack).py

--- uri 1038 solution (Snack).py
+++ uri 1038 solution (Snack).py
@@ -1,4 +1,13 @@
 #solution of uri onlinejudge problem no.1038 (Snack)
+#with for loop::(faster)
+x,y=map(int,input().split())
+v={1:4.00,2:4.50,3:5.00,4:2.00,5:1.50}
+for i in v:
+    if x==i:
+        p=v[i]*y
+print('Total: R$ %.2f'%p)
+
+#with if condition::
 x,y=map(int,input().split())
 if x==1:
     v=4.00*y
