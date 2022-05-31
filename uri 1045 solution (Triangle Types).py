a3eac517453510771f4f9317b89d447b6efd3993
ni-opu <67326422+ni-opu@users.noreply.github.com>
2022-05-31 19:55:00 +0600

    Create uri 1045 solution (Triangle Types).py

---
+++ uri 1045 solution (Triangle Types).py
@@ -0,0 +1,13 @@
+a,b,c=sorted(list(map(float,input().split())),reverse=True)
+if a>=b+c:
+    print('NAO FORMA TRIANGULO')
+elif a**2==(b**2+c**2):
+    print('TRIANGULO RETANGULO')
+elif a**2>(b**2+c**2):
+    print('TRIANGULO OBTUSANGULO')
+elif a**2<(b**2+c**2):
+    print('TRIANGULO ACUTANGULO')
+if a==b==c:
+    print('TRIANGULO EQUILATERO')
+elif a==b or b==c:
+    print('TRIANGULO ISOSCELES')
